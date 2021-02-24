6abaccd58f0a66140fa6394e7e338dd05bc941aa
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-02-24 12:48:57 +0100

    Update to webpacker 6
    [ci skip]

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,17 +1,31 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const environment = require('./environment')
+const { merge } = require('@rails/webpacker')
+const webpackConfig = require('./base')
+
 const StyleLintPlugin = require('stylelint-webpack-plugin')
 
-environment.plugins.append(
-  'StyleLintPlugin',
-  new StyleLintPlugin({
-    files: '/app/**/*.(s(c|a)ss|css)'
-  })
-)
+const customConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /vendor\/.+\.js$/,
+        loader: 'standard-loader',
+        options: {
+          error: true,
+          globals: [
+            '$'
+          ]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new StyleLintPlugin({
+      files: '/app/**/*.(s(c|a)ss|css)'
+    })
+  ]
+}
 
-environment.loaders.get('babel').use.push({
-  loader: 'standard-loader', options: { error: true }
-})
-
-module.exports = environment.toWebpackConfig()
+module.exports = merge(webpackConfig, customConfig)
